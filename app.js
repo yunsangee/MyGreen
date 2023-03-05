e8ff7fe1c8f281b48da8d83cee35f81012eed7b3
DEMl4N <delfirmamentos@gmail.com>
2023-03-05 21:11:49 +0900

    03-05

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send("MyGreen ^^")
+})
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.post('/test', (req, res) => {
+  const {name, memo} = req.body
+  const title = "Connection Test"
+  console.log(title)
+  const servertime = new Date().toTimeString();
+  const message = `Your name is ${name} and your memo is ${memo}`
+  const response = { title, servertime, message };
+  res.json(response);
+})
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = app
