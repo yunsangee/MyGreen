ffe60d9f4f071b6a6c04bc0e6c0f497e5ce9c81c
DEMl4N <delfirmamentos@gmail.com>
2023-03-11 16:15:47 +0900

    example for babykingkong

--- routes/account/create.js
+++ routes/account/create.js
@@ -3,6 +3,7 @@ var router = express.Router()
 
 router.post('/', (req, res) => {
     // create code here
+    console.log(req.body.id, req.body.password, req.body.name, req.body.birth, req.body.gender, req.body.email)
 })
 
 module.exports = router
