86f10910651b9da9ee1b79b8911e4e6681cf7d26
DEMl4N <delfirmamentos@gmail.com>
2023-03-16 17:02:50 +0900

    Feat: DB Module

--- db/database.js
+++ db/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const database = require('../config/database')
 
-mongoose.connect(database.local + '/my_green')
+//mongoose.connect(database.local + '/my_green')
+mongoose.connect(database.remote + '/my_green')
 .then(() => console.log("DB Connected"))
 .catch(error => console.log(error))
 
