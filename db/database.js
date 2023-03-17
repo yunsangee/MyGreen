f56aa4985fdd7abfa731765dec1c4c49a23ef397
DEMl4N <delfirmamentos@gmail.com>
2023-03-17 22:37:01 +0900

    Fix: Connection String

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const database = require('../config/database')
 
 //mongoose.connect(database.local + '/my_green')
-mongoose.connect(database.remote + '/my_green')
+mongoose.connect(database.remote)
 .then(() => console.log("DB Connected"))
 .catch(error => console.log(error))
 
