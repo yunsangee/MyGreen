f9b0e13efc0a1f06c4084f55c7d58e9ed3973747
DEMl4N <delfirmamentos@gmail.com>
2023-03-19 17:08:06 +0900

    Fix: Add error res

--- routes/account/login.js
+++ routes/account/login.js
@@ -23,12 +23,12 @@ router.post('/', (req, res) => {
     };
     
     login(req.body.id, req.body.password)
-    .then(status => {
-        if (status) {
-            res.send("GOOOD!")
+    .then(result => {
+        if (!result) {
+            res.status(406).send("Not Good")
         }
         else {
-            res.status(406)
+            res.send("GOOOD!")
         }
     })
 })
