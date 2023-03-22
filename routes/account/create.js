240ec4d9e8aaeb42f968b65d57b64bb3af8b2f68
yunsangee <121428465+yunsangee@users.noreply.github.com>
2023-03-22 15:31:20 +0900

    Update create.js

--- routes/account/create.js
+++ routes/account/create.js
@@ -4,25 +4,20 @@ const user = require('../../db/models/user')
 var router = express.Router()
 
 router.post('/', (req, res) => {
-    if (!req.body.id || !req.body.password || !req.body.name || !req.body.email || !req.body.birth) {
-        console.log("There are spaces missing")
-        return res.status(400).send("Bad Request")
-    }
 
+    if(!id || !password || !name || !email || !birth){
+        console.log("There are spaces missing")
+    }
     user.create({
         id: req.body.id,
         password: req.body.password,
         name: req.body.name,
         email: req.body.email,
         birth: req.body.birth
-    }, (err, data) => {
-        if (err) {
-            console.error(err)
-            return res.status(500).send("Server Error")
-        }
-        console.log(data)
-        res.send("Account Created! " + req.body.id);
-    });
+    })
+
+    console.log(req.body.id, req.body.password, req.body.name, req.body.birth, req.body.email)
+    res.send("Account Created! " + req.body.id)
 })
 
 module.exports = router
