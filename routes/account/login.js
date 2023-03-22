d0af3665668be76a27189d269610f26324b7a528
DEMl4N <delfirmamentos@gmail.com>
2023-03-22 15:29:22 +0900

    Fix: 신윤상이 발뺌함

--- routes/account/login.js
+++ routes/account/login.js
@@ -8,11 +8,13 @@ router.post('/', (req, res) => {
           const status = await user.findOne({ 
             id: id,
             password: password
-         })
-         .exec();
+        })
+        .exec();
+
         if (!status) {
             console.log("ID not found");
         }
+        
         return status
         } catch (err) {
             console.error(err);
@@ -20,15 +22,11 @@ router.post('/', (req, res) => {
     };
 
     login(req.body.id, req.body.password)
-    .then(status => {
-        if (status) {
-            res.send("GOOOD!")
     .then(result => {
         if (!result) {
             res.status(406).send("Not Good")
         }
         else {
-            res.status(406)
             res.send("GOOOD!")
         }
     })
