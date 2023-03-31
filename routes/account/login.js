82d91a6870f8533f44a9f1c9afd13573c7657a7c
DEMl4N <delfirmamentos@gmail.com>
2023-03-31 22:03:09 +0900

    Fix: Conflict in routes/account/login.js

--- routes/account/login.js
+++ routes/account/login.js
@@ -6,40 +6,10 @@ router.post('/', (req, res) => {
     const login = async (id, password) => {
         try {
           const status = await user.findOne({ 
-<<<<<<< HEAD
             id: id,
             password: password
         })
         .exec();
-=======
-            id: req.body.id,
-            password: req.body.password
-       })
-         .exec();
-
-         if (!user) throw new Error('Invalid login credentials');
-         const isPasswordValid = await user.checkPassword(req.body.password);
-         if (!isPasswordValid) throw new Error('Invalid login credentials');
-         res.status(200).send('Login successful');    
-
-
-        return status
-        } catch (err) {
-            res.status(401).send(err.message)
-        }
-    };
-    
-    login(req.body.id, req.body.password)
-    .then(result => {
-        if (!result) {
-            res.status(406).send("Not Good")
-        }
-        else {
-            res.send("GOOOD!")
-        }
-    })
-})
->>>>>>> origin/ys
 
         if (!status) {
             console.log("ID not found");
